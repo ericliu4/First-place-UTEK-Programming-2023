ff95dec8726b96efd9ee2375a471af58b5294bb0
spoink2022 <spoink2022@gmail.com>
2023-11-11 13:59:33 -0500

    q3 finished (crude)

--- q3/q3.py
+++ q3/q3.py
@@ -3,7 +3,13 @@ import copy
 
 def read_input(filename):
     with open(filename, 'r') as f:
-        line = f.read().strip().split(' , ')
+        line = f.readline().strip()
+        start_node = line[line.index('"')+1:-1]
+        line = f.readline().strip()
+        end_node = line[line.index('"')+1:-1]
+        line = f.readline().strip()
+        maxTime = int(line[line.index('=')+2:-3])
+        line = f.readline().strip().split(' , ')
     letters = {}
     parsed = []
     letters_arr = []
@@ -11,14 +17,12 @@ def read_input(filename):
 
     for chunk in line:
         chunk = chunk.strip()
-        print(chunk)
         index = len(chunk) - 1
 
         while chunk[index] != ',':
             index -= 1
         cooldown = int(chunk[index + 2:chunk[index+2:].index(' ') + index + 2])
         comma_index = index
-        print(cooldown)
 
         while chunk[index] != '$':
             index -= 1
@@ -41,49 +45,73 @@ def read_input(filename):
 
         parsed.append([letters[node1], letters[node2], cost, cooldown])
 
-    return parsed, letters_arr
+    start_index = letters[start_node]
+    end_index = letters[end_node]
+    return start_index, end_index, maxTime, parsed, letters_arr
 
 
-def question3Main(startingNode, endingNode, edges, n):
+def question3Main(startingNode, endingNode, maxTime, edges, n):
     # Initialize graph
     graph = collections.defaultdict(list)
-    for node1, node2, cost in edges:
-        graph[node1].append([node2, cost])
+    for node1, node2, cost, t in edges:
+        graph[node1].append([node2, cost, t])
 
     # Initialize variables
     lowestCost = float('inf')
     lowestPath = []
+    lowestTime = 0
     queue = collections.deque()
     start = 1 << startingNode
     pathStart = []
     pathStart.append(startingNode)
-    queue.append((startingNode, start, 0, pathStart))
+    queue.append((startingNode, start, 0, 0, pathStart))
     end = (1 << n) - 1 
-    seen = collections.defaultdict(lambda: float('inf'))
-    
+
+    #seen = collections.defaultdict(lambda: {maxTime: float('inf')})
+    seen = {}
+
     # BFS with bitmasking
     while queue:
-        currNode, currMask, weight, currPath = queue.popleft()
+        currNode, currMask, weight, currTime, currPath = queue.popleft()
         #print(currNode, bin(currMask), weight)
         #print(currPath)
         if currMask == end and currNode == endingNode:
             if lowestCost > weight:
                 lowestCost = weight
                 lowestPath = currPath
+                lowestTime = currTime
             continue
         
-        for neighbor, edgeWeight in graph[currNode]:
+        for neighbor, edgeWeight, edgeTime in graph[currNode]:
             neighborBit = 1 << neighbor
             nextMask = currMask | neighborBit
             newWeight = weight + edgeWeight
-            if newWeight < seen[(neighbor, nextMask)]:
+            newTime = currTime + edgeTime
+
+            # Ignores the possibility if time exceeded
+            if newTime > maxTime:
+                continue
+            
+            # Create empty dictionary if it is not in seen yet
+            if (neighbor, nextMask) not in seen:
+                seen[(neighbor, nextMask)] = {}
+
+            # Loop over values in seen[(neighbor, nextMask)] to see if we have a valid point
+            is_better = True
+            for key, val in seen[(neighbor, nextMask)].items(): # key => time, val => cost
+                if newWeight >= val and newTime >= key: # better path exists already
+                    is_better = False
+                    break
+            
+            if is_better: # update seen
                 currPath.append(neighbor)
-                seen[(neighbor, nextMask)] = newWeight
-                queue.append((neighbor, nextMask, newWeight, copy.copy(currPath)))
+                seen[(neighbor, nextMask)][newTime] = newWeight
+                queue.append((neighbor, nextMask, newWeight, newTime, copy.copy(currPath)))
                 currPath.pop()
+
     if lowestCost != float('inf'):
-        return lowestCost, lowestPath
-    return -1, ""
+        return lowestCost, lowestPath, lowestTime
+    return -1, "", ""
 
 def to_human_readable(best_path, node_names):
     return ', '.join([f'{node_names[best_path[i]]}->{node_names[best_path[i+1]]}' for i in range(len(best_path) - 1)])
@@ -91,8 +119,8 @@ def to_human_readable(best_path, node_names):
 
 if __name__ == '__main__':
     #take input file
-    file = "a.in"
-    startNode, endNode, edgesList, letterMap = read_input(file)
+    file = "inputs/a3.in"
+    startNode, endNode, maxTime, edgesList, letterMap = read_input(file)
     #print(startNode)
     #print(endNode)
     #print(edgesList)
@@ -103,10 +131,11 @@ if __name__ == '__main__':
     #print("\n\n")
 
 
-    cost, path = question3Main(startNode, endNode, edgesList, len(letterMap))
+    cost, path, travelTime = question3Main(startNode, endNode, maxTime, edgesList, len(letterMap))
     #print("\n\n")
     s = to_human_readable(path, letterMap)
     print(f'Best path: {s}')
     print(f'Cost: ${cost}')
+    print(f'Time: {travelTime}')
     #print("\n\n")
 
